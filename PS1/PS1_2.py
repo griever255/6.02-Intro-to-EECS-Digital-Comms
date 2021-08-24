78c72d07bcd28e21f7c45c01937196572d9e9751
griever255 <jgargiulo95@gmail.com>
2021-08-24 18:10:40 -0400

    Finished PS1_2

--- PS1/PS1_2.py
+++ PS1/PS1_2.py
@@ -16,9 +16,16 @@ def decode(encoding_dict,encoded_message):
     encoded_msg: [1, 1, 0, 1, 0, 0, 1, 0, 1]
     returns 'ABCD'
     """
-    # Your code here
-    pass
-
+    msg = []
+    buffer = []
+    keys = list(encoding_dict.keys())
+    values = list(encoding_dict.values())
+    for bit in encoded_message:
+       buffer.append(bit)
+       if buffer in values:
+              msg.append(keys[values.index(buffer)])
+              buffer = []
+    return msg
 
 if __name__ == '__main__':
     # start by building Huffman tree from probabilities
@@ -34,7 +41,7 @@ if __name__ == '__main__':
            (message,decoded_message)
 
     # test case 2: construct a random message and encode it
-    message = [random.choice('ABCD') for i in xrange(100)]
+    message = [random.choice('ABCD') for i in range(100)]
     encoded_message = PS1_tests.encode(cdict,message)
     decoded_message = decode(cdict,encoded_message)
     assert message == decoded_message, \
