a4e4a30f3daa09f93088a6ca2ee42a5a10418a35
griever255 <jgargiulo95@gmail.com>
2021-09-12 15:51:23 -0400

    Finished PS2 rectparity.py

--- PS2/PS2_rectparity.py
+++ PS2/PS2_rectparity.py
@@ -1,11 +1,55 @@
 # template for 6.02 rectangular parity decoding using error triangulation
 import PS2_tests
-import numpy
+import numpy as np
 
 def rect_parity(codeword,nrows,ncols): 
-    ## YOUR CODE HERE
-    ## return the corrected data
-    return
+    bits = (nrows+1)*(ncols+1)-1
+    packet = np.array([[0]*(ncols+1)]*(nrows+1))
+    bit = 0
+    for row in range(nrows):
+        for col in range(ncols):
+            packet[row][col] = codeword[bit]
+            bit += 1
+    for row in range(nrows):
+        packet[row][ncols] = codeword[bit]
+        bit += 1
+    for col in range(ncols):
+        packet[nrows][col] = codeword[bit]
+        bit += 1
+
+    # Get the row, col of all parity errors 
+    col_sums = packet.sum(axis=0)%2
+    row_sums = packet.sum(axis=1)%2
+    error_locs = []
+    for r_index, r_parity in enumerate(row_sums):
+        for c_index, c_parity in enumerate(col_sums):
+            if c_parity == 1 and r_parity == 1:
+                error_locs.append([r_index,c_index])
+    
+    # Remove parity bit errors
+    for row in range(nrows):
+        for col in range(ncols):
+            if [row, ncols] in error_locs:
+                error_locs.remove([row, ncols])
+            elif [nrows, col] in error_locs:
+                error_locs.remove([nrows, col])
+            elif [nrows, ncols] in error_locs:
+                error_locs.remove([nrows, ncols])
+
+    # Default is to return the codeword data
+    message = codeword[:(nrows*ncols)]
+
+    # Correct all single-bit errors
+    if len(error_locs) == 1:
+        index = error_locs[0]
+        if index[0] < nrows and index[1] < ncols:
+            bit = index[0]*ncols + index[1]
+            if packet[index[0]][index[1]] == 0:
+                codeword[bit] = 1
+            else:
+                codeword[bit] = 0
+            message = codeword[:(nrows*ncols)]
+    return message
 
 if __name__ == '__main__':
     PS2_tests.test_correct_errors(rect_parity)
--- PS2/PS2_tests.py
+++ PS2/PS2_tests.py
@@ -2,6 +2,7 @@
 import operator
 import math,numpy,random
 from numpy import *
+from functools import reduce
 
 # return True if the binary sequence contains an even number of 1's
 def even_parity(seq):
@@ -17,46 +18,46 @@ def ber(xmit,received):
 def codeword(data,nrows,ncols):
     result = data[:]
     # compute row parity bits
-    for r in xrange(nrows):
+    for r in range(nrows):
         result.append(int(not even_parity(data[r*ncols:(r+1)*ncols])))
     # compute column parity bits
-    for c in xrange(ncols):
+    for c in range(ncols):
         result.append(int(not even_parity(data[c:len(data):ncols])))
     return result
 
 # convert integer to binary list of specified size (lsb first)
 def int2bin(c,size=8):
-    return [(c >> i) % 2 for i in xrange(size)]
+    return [(c >> i) % 2 for i in range(size)]
 
 def test_correct_errors(correct_errors):
     # test the decoder on a particular value, returns True if failed
     def run_test(test,nrows,ncols,expected,msg):
         result = correct_errors(test[:],nrows,ncols)
         if result != expected:
-            print "Error detected while testing",msg
-            print "Test code word:",test
-            print "Expected:",expected
-            print "Received:",result
+            print("Error detected while testing",msg)
+            print("Test code word:",test)
+            print("Expected:",expected)
+            print("Received:",result)
             return True
         return False
 
     # test all 256 data values for 8 data bits
-    print 'Testing all 2**n = 256 valid codewords'
-    for i in xrange(256):
+    print('Testing all 2**n = 256 valid codewords')
+    for i in range(256):
         data = int2bin(i)
         if run_test(codeword(data,2,4),2,4,data,"valid codewords"): return
-    print '...passed'
+    print('...passed')
 
     # test all single-bit errors (should be corrected)
-    print 'Testing all possible single-bit errors'
+    print('Testing all possible single-bit errors')
     for count in range(10):
         good = codeword(int2bin((count*37 + 15) % 256),4,2)
-        for i in xrange(14):
+        for i in range(14):
             bad = good[:]
             bad[i] ^= 1
             if run_test(bad,4,2,good[0:8],"correctable single-bit errors (bit %d)" % i): return
-    print '...passed'
-    print 'Testing all possible two-bit errors'
+    print('...passed')
+    print('Testing all possible two-bit errors')
     for i in range(256):                    # all possible 8-bit messages
         good = codeword(int2bin(i),2,4)     # form a valid codeword, mimic TX
         for j in range(14):                 # 14 bits in total
@@ -73,8 +74,8 @@ def test_correct_errors(correct_errors):
                else :
                  if run_test(bad2,2,4,bad2[0:8],"Two bit errors @(" +str(j)+ "," + str(k)+")" ): return
                                             # best effort here. Just return bits as is. 
-    print '...passed'
-    print '(8,4) rectangular parity code successfully passed all 0,1 and 2 bit error tests'
+    print('...passed')
+    print('(8,4) rectangular parity code successfully passed all 0,1 and 2 bit error tests')
 
 # construct a codeword with row and column parity
 def codeword2(data,nrows,ncols):
@@ -83,10 +84,10 @@ def codeword2(data,nrows,ncols):
     result = numpy.zeros(ndata + nrows + ncols,dtype=int)
     result[:ndata] = data
     # compute row parity bits
-    for r in xrange(nrows):
+    for r in range(nrows):
         result[ndata + r] = even_parity(data[r*ncols:(r+1)*ncols])
     # compute column parity bits
-    for c in xrange(ncols):
+    for c in range(ncols):
         result[ndata + nrows + c] = even_parity(data[c:len(data):ncols])
     return result
 
@@ -100,9 +101,9 @@ def test_stream_decode(decoder,verbose=True):
         cw.extend(codeword2(message[i:i+nrows*ncols], nrows, ncols))
     result = decoder(numpy.array(cw), nrows, ncols)
     if numpy.any(result != message):
-        print "Error detected while testing", message
-        print "Decoded wrongly as", result
-        print "The correct codeword is", cw
+        print("Error detected while testing", message)
+        print("Decoded wrongly as", result)
+        print("The correct codeword is", cw)
         return False
 
     nrows = 4
@@ -114,12 +115,12 @@ def test_stream_decode(decoder,verbose=True):
         cw.extend(codeword2(message[i:i+nrows*ncols], nrows, ncols))
     result = decoder(cw, nrows, ncols)
     if numpy.any(result != message):
-        print "Error detected while testing", message
-        print "Decoded wrongly as", result
-        print "The correct codeword is", cw
+        print("Error detected while testing", message)
+        print("Decoded wrongly as", result)
+        print("The correct codeword is", cw)
         return False
 
-    if verbose: print "***Stream decoding succeeded***"
+    if verbose: print("***Stream decoding succeeded***")
     return True
 
 # For testing: generate all codewords of n bits in a list of lists, where each
@@ -160,18 +161,18 @@ def all_valid_codewords(n, k, G):
     return mod2(C)
 
 def test_linear_sec(decoder, n, k, G):
-    print 'Testing all 2**k =', 2**k, 'valid codewords'
+    print('Testing all 2**k =', 2**k, 'valid codewords')
     C = all_valid_codewords(n, k, G)
     for i in range(2**k):
         cw = numpy.array(C[i:i+1].flatten()[0].tolist()[0])
         d = decoder(cw, n, k, G)
         if not eqlist(d, cw[:k]):
-            print 'In code with generator matrix', G
-            print 'Error decoding', cw, 'Expected', cw, 'got', d
+            print('In code with generator matrix', G)
+            print('Error decoding', cw, 'Expected', cw, 'got', d)
             return
 #        print cw, '... passed'
-    print '...passed'
-    print 'Testing all n*2**k =', n*2**k, 'single-bit error codewords'
+    print('...passed')
+    print('Testing all n*2**k =', n*2**k, 'single-bit error codewords')
     for i in range(2**k):
         cw = numpy.array(C[i:i+1].flatten()[0].tolist()[0])
         for j in range(n):
@@ -180,12 +181,12 @@ def test_linear_sec(decoder, n, k, G):
             storetest = 1*test
             d = decoder(test, n, k, G)
             if not eqlist(d, cw[:k]):
-                print 'In code with generator matrix'
-                print G
-                print 'OOPS: Error decoding', storetest, '...expected', cw[:k], 'got', d
+                print('In code with generator matrix')
+                print(G)
+                print('OOPS: Error decoding', storetest, '...expected', cw[:k], 'got', d)
                 return
 #            print test, '... passed'
             
-    print '...passed'
-    print "All 0 and 1 error tests passed for (%d,%d,3) code with generator matrix G =" % (n, k)
-    print G
+    print('...passed')
+    print("All 0 and 1 error tests passed for (%d,%d,3) code with generator matrix G =" % (n, k))
+    print(G)
