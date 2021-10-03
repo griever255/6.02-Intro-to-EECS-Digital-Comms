11ab75f49121f23f2829acd1a0e80723d8ebb3bb
griever255 <jgargiulo95@gmail.com>
2021-10-03 14:13:58 -0400

    finished PS3 hard-decision

--- PS3/PS3_viterbi.py
+++ PS3/PS3_viterbi.py
@@ -39,7 +39,7 @@ class ViterbiDecoder:
           [[PS3_tests.expected_parity(s1, s2, K, glist) \
             if s1 in self.predecessor_states[s2] else None
             for s2 in range(self.nstates)]
-           for s1 in range(self.nstates)]
+            for s1 in range(self.nstates)]
 
     # expected is an r-element list of the expected parity bits.
     # received is an r-element list of actual sampled voltages for the
@@ -50,8 +50,15 @@ class ViterbiDecoder:
     # branch metric.  Consider using PS3_tests.hamming(seq1,seq2) which
     # computes the Hamming distance between two binary sequences.
     def branch_metric(self,expected,received):
-        #your code here
-        pass
+        ham_dist = 0
+        for index, value in enumerate(received):
+            if value >= 0.5:
+                value = 1
+            else:
+                value = 0
+            if value != expected[index]:
+                ham_dist +=1
+        return ham_dist
 
     # Compute self.PM[...,n] from the batch of r parity bits and the
     # path metrics for self.PM[...,n-1] computed on the previous
@@ -64,8 +71,29 @@ class ViterbiDecoder:
     # and self.branch_metric().  To see what these mean, scan through the
     # code above.
     def viterbi_step(self,n,received_voltages):
-        #your code here
-        pass
+        # At each t_step, n, determine the two predecessaor states
+        # for each state, s
+        for s, p_states in enumerate(self.predecessor_states):
+            alpha, beta = p_states
+            
+            # Determine pathmetrics of each p_state
+            PM_alpha = self.PM[alpha, n-1]
+            PM_beta = self.PM[beta, n-1]
+            
+            # Determine the BM of each p_state
+            expected_alpha = self.expected_parity[alpha][s]
+            expected_beta = self.expected_parity[beta][s]
+            BM_alpha = self.branch_metric(expected_alpha, received_voltages)
+            BM_beta = self.branch_metric(expected_beta, received_voltages)
+            
+            # Determine the most-likely predecessor by minimum BM
+            if BM_alpha <= BM_beta:
+                self.Predecessor[s][n] = alpha
+            else:
+                self.Predecessor[s][n] = beta
+
+            # Determine the PM for each state, s, at time step n
+            self.PM[s, n] = min(PM_alpha+BM_alpha, PM_beta+BM_beta)
 
     # Identify the most-likely ending state of the encoder by
     # finding the state s that has the minimum value of PM[s,n]
@@ -73,8 +101,7 @@ class ViterbiDecoder:
     # are several states with the same minimum value, choose one
     # arbitrarily.  Return the state s.
     def most_likely_state(self,n):
-        #your code here
-        pass
+        return numpy.argmin(self.PM[:,n])
 
     # Starting at state s at time n, use the Predecessor
     # array to find all the states on the most-likely
@@ -82,21 +109,28 @@ class ViterbiDecoder:
     # the trellis).  Each state contributes a message bit.
     # Return the decoded message as a sequence of 0's and 1's.
     def traceback(self,s,n):
-        #your code here
-        pass
+        msg = numpy.zeros(n)
+        s = format(s, "0"+str(self.nstates.bit_length()-1)+"b")
+        msg[n-1] = s[0]
+        for time in range(n, 1, -1):
+            s = self.Predecessor[self.most_likely_state(time),time]
+            s = format(s, "0"+str(self.nstates.bit_length()-1)+"b")
+            msg[time-2] = s[0]
+        return msg
+
 
     # Figure out what the transmitter sent from info in the
     # received voltages.
     def decode(self,received_voltages,debug=False):
         # figure out how many columns are in the trellis
         nreceived = len(received_voltages)
-        max_n = (nreceived/self.r) + 1
+        max_n = (nreceived//self.r) + 1
 
         # this is the path metric trellis itself, organized as a
         # 2D array: rows are the states, columns are the time points.
         # PM[s,n] is the metric for the most-likely path through the
         # trellis arriving at state s at time n.
-        self.PM = numpy.zeros((self.nstates,max_n),dtype=numpy.float)
+        self.PM = numpy.zeros((self.nstates,max_n),dtype=float)
 
         # at time 0, the starting state is the most likely, the other
         # states are "infinitely" worse.
@@ -106,7 +140,7 @@ class ViterbiDecoder:
         # points, contents indicate the predecessor state for each
         # current state.
         self.Predecessor = numpy.zeros((self.nstates,max_n),
-                                       dtype=numpy.int)
+                                       dtype=int)
 
         # use the Viterbi algorithm to compute PM
         # incrementally from the received parity bits.
@@ -151,13 +185,14 @@ class ViterbiDecoder:
 if __name__=='__main__':
     constraint_len = 3; glist = (7,5,3)
     d = ViterbiDecoder(constraint_len, glist)
-
+    
     # first test case: example from lecture
     message = [1,0,1,0,1,1,0,0]
     received = PS3_tests.convolutional_encoder(message, constraint_len, glist)
     i = 0
     print('TEST', i)
     decoded = numpy.array(d.decode(received, debug=True))
+    
     print('Testing without adding noise...')
     if (message == decoded).all() == True: 
         print('Successfully decoded no-noise Test 0: congratulations!')
