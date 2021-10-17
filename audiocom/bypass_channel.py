766020aa9b046d1c8499f3fca88b78a080107260
griever255 <jgargiulo95@gmail.com>
2021-10-16 22:03:55 -0400

    created xmit noise

--- audiocom/bypass_channel.py
+++ audiocom/bypass_channel.py
@@ -14,4 +14,5 @@ class BypassChannel:
         random.seed()
 
     def xmit_and_recv(self, tx_samples):
-        raise NotImplementedError("xmit_and_recv")
+        noise_array = numpy.random.normal(0, self.noise**(1/2), tx_samples.shape)
+        return tx_samples + noise_array
